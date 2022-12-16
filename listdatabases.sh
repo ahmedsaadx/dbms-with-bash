a84233ef932923b289277e5fd83142ed84aed2b6
ahmedsaadx <ahmeddsaadd188@gmail.com>
2022-12-16 17:19:36 +0200

    modify list feature

--- listdatabases.sh
+++ listdatabases.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/bash 
+declare -a lists
 list(){
   dir=`ls -l | grep '^d' |cut -d' ' -f9`
   for i in $dir 
   do 
-  lastlist=(${lastlist[@]} $i)
-  echo $i
+  lists=(${lists[@]} $i)
   done 
-}
-list
+}  
+
+ check (){
+    if (( "${#lists[@]}" == 0 )) ;
+  then
+    echo -e '\nno there databases yet\nyou can create new database \nDo you want to create? :'
+    select answer in 'yes' 'no'
+    do 
+     case $answer in 
+       "yes") . ./createdatabase.sh ;exit
+       ;;
+       "no") exit ;;
+       
+       *) echo "please answer with Y/N " 
+       ;;
+    esac    
+    done
+      
+  else 
+     echo $i
+     
+   fi  
+ }
+ list
+ check
+
 read -p 'went back to main menu [Yy , Nn for exit] :' choice
 case $choice in 
    ['Y-y']) menu 
    ;;
    ['N-n']) exit 
    ;;
-   *) echo  -e '\nplease enter yes or no only \n\nyour databases list again: \n' ;list;;
+   *) echo  -e '\nplease enter yes or no only \n\nyour databases list again: \n' ;check;;
 esac
  
