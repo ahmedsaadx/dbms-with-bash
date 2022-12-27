faf8e3732d8d6052d5f0731892c3c81bc0fd7fb8
ahmedsaadx <ahmeddsaadd188@gmail.com>
2022-12-27 15:34:16 +0200

    add insert and update  features

---
+++ insert.sh
@@ -0,0 +1,62 @@
+
+
+regex='A-Za-z'
+regex_num='1-9'
+add_record(){
+    col_num=`awk "END{print NR}" $tname.rows`
+    
+    i=1
+    while (( i <= $col_num ))
+    do
+    declare -a record
+    checkcolname=`awk 'BEGIN {FS=":"}{if ( NR=='$i' ) print $1 }' $tname.rows`
+    checkcolPK=`awk 'BEGIN {FS=":"}{if ( NR=='$i' ) print $2 }' $tname.rows`
+    checkcoltype=`awk 'BEGIN {FS=":"}{if ( NR=='$i' ) print $3 }' $tname.rows`
+    echo "enter your record $checkcolname :"
+    read -r record_
+    if [[ $checkcoltype == 'int' ]] ;
+    then
+     while [[ $record_ =~ [^$regex_num] ]];
+     do
+        echo " error [you must enter numbers only] "
+        echo "enter your record again"
+        read -r record_
+        
+    done
+    elif [[ $checkcoltype == 'str' ]];
+    then
+       while  [[ $record_ =~ [^$regex] ]];
+       do
+        echo " error [you must enter english char only] "
+        echo "enter your record again"
+        read -r record_
+        
+        done
+    fi
+    if [[ $checkcolPK == 'yes' ]];
+    then
+    ahmed=`awk -v var=$i -F : 'BEGIN{ORS=" "} {print $var}' $tname`
+    for item in $ahmed
+    do 
+        if  [ $item = $record_ ];
+         then 
+            echo 'primary key must be uniqe '
+            exit
+        fi
+    done 
+    fi  
+    record=(${record[@]} $record_ ':')
+    i=$(( $i + 1 ))
+    done
+    echo ${record[@]} >> $tname
+   
+}
+echo "enter table name : "
+read -r tname 
+
+if [[ -e $tname ]];
+then
+    add_record
+else
+   echo "table don't exist "
+fi
---
+++ update.sh
@@ -0,0 +1,97 @@
+update_(){
+    echo "enter your column name :"
+    read -r column_
+    col_num=`awk 'END{print NR }' $tname.rows`
+    i=1
+    while(( $i <= $col_num ))
+    do
+    vaild_column_name=`awk -F : '{if ( NR == '$i') print $1 }' $tname.rows`
+    if [ $column_ == $vaild_column_name ];
+    then
+        column_type=`grep $vaild_column_name $tname.rows | cut -d : -f 3 ` 
+        column_pk=`grep $vaild_column_name $tname.rows | cut -d : -f 2`
+        echo "enter your new value :"
+        read -r value_
+        if [ -z $value_ ];
+        then
+        echo 'value must not be empty'
+        exit
+        elif [ $column_pk = 'yes' ];
+        then
+        ahmed=`awk -v var=$i -F : 'BEGIN{ORS=" "} {print $var}' $tname`
+        for ii in $ahmed
+                do
+                    if [ $value_ = $ii ];
+                    then
+                        echo "invalid input (pk must be unqiue)"
+                        exit 
+                     fi      
+                done
+ 
+        fi
+        echo "enter your old value"
+        read -r oldvalue
+        
+        if [ $column_type = 'int' ];
+        then
+            if [[  $value_ =~ [^0-9] ]];
+            then
+                echo " error enter numbers only"
+            else    
+            ahmed=`awk -v var=$i -F : 'BEGIN{ORS=" "} {print $var}' $tname`
+                for ii in $ahmed
+                do
+                    if [ $oldvalue == $ii ];
+                    then
+                        sed -i 's/'$oldvalue/$value_'/'  $tname
+                        break
+                     fi   
+                done
+            fi
+        elif [ $column_type = 'str' ];
+        then
+            if [[  $value_ =~ [^A-Za-z] ]];
+            then
+                echo " error enter char only"
+            else   
+            ahmed=`awk -v var=$i -F : 'BEGIN{ORS=" "} {print $var}' $tname`
+                for ii in $ahmed
+                do
+                    if [ $oldvalue = $ii ];
+                    then
+                        sed -i 's/'$oldvalue/$value_'/'  $tname
+                        break
+                     fi      
+                done
+                fi 
+       
+            
+                
+        fi
+        
+
+    fi
+    
+    i=$(( $i + 1 ))
+    done
+}
+
+
+
+echo "enter table name : "
+read -r tname
+if [[ -e $tname ]];
+then    
+    update_
+else
+   echo "table don't exist "
+fi
+
+
+
+
+
+
+
+
+
